814b567180435a795c0085b0e2cdd6d5ddf3a733
Sheikh sadi <you@example.com>
2022-09-08 03:11:08 -0700

    a

--- setTimeOut.js
+++ setTimeOut.js
@@ -1,3 +1,5 @@
+/* setTimeout এর ভিতরে একটা কিছু কনসোল লগ করবে যেটা সাড়ে তিন সেকেন্ড পরে আউটপুট দেখাবে।  */
+
 console.log('first');
 setTimeout(() => console.log('two'),3000 ) // this will be come after 3 second
 console.log('third');
